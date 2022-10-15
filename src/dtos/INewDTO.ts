3cb10035071adf0ba571956218d013bfd0194197
André Quintino <andre.quintino@mindswap.academy>
2022-10-15 17:27:18 +0100

    refactor(dtos): add source prop

--- src/dtos/INewDTO.ts
+++ src/dtos/INewDTO.ts
@@ -1,7 +1,13 @@
+enum EClientsApisUrls {
+	algolia = 'http://hn.algolia.com/api/v1',
+	newsapi = 'https://newsapi.org/v2',
+}
+
 interface INewDTO {
 	title: string;
 	url: string;
 	author: string;
+	source: EClientsApisUrls;
 }
 
 export { INewDTO };
