b4167afbdb644d6d1d7148916215dfe4ae7ecc91
André Quintino <andre.quintino@mindswap.academy>
2022-10-15 17:27:47 +0100

    refactor(mappers): add source prop

--- src/mappers/newMapper.ts
+++ src/mappers/newMapper.ts
@@ -1,7 +1,12 @@
 import { INewDTO } from '../dtos/INewDTO';
 
 function convertToNewDTO(json: any): INewDTO {
-	return { title: json.title, author: json.author, url: json.url };
+	return {
+		title: json.title,
+		author: json.author,
+		url: json.url,
+		source: json.source,
+	};
 }
 
 export { convertToNewDTO };
